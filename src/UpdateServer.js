8e8fa244e4b606299fdd01869133f4c46246d4e7
Unknown <ihachroblox123@gmail.com>
2017-06-11 19:46:51 +0200

    Added

---
+++ src/UpdateServer.js
@@ -0,0 +1,30 @@
+const express       = require('express')
+const DiscordServer = require('./DiscordServer')
+
+module.exports = 
+// This function starts the update server. This is used
+// to cause the bot to download new information about a
+// target user and update the user's state in all servers.
+function(discordBot, config) {
+    let server = express();
+
+    server.get('/update-user', (req, res) => {
+        if (config.apiKey && config.apiKey !== req.query.apiKey) {
+            res.status(403).end();
+        }
+
+        if (req.query.id == null) {
+            res.status(400).end();
+        }
+
+        let id = req.query.id;
+
+        res.end('ok');
+
+        discordBot.globallyUpdateMember(id);
+    })
+
+    server.listen(config.port, () => {
+        console.log(`Update server listening on ${config.port}`);
+    });
+}
